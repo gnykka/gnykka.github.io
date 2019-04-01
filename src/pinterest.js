283fa197d9fedc529d4c4f5e954d8a55c2d5a6f1
Nataliya Stepanova <gnykka@gmail.com>
2019-04-01 12:57:45 +0300

    Initial commit with pinterest demo

---
+++ src/pinterest.js
@@ -0,0 +1,12 @@
+(() => {
+  window.addEventListener('load', () => {
+    const main = document.getElementsByTagName('main')[0];
+
+    // make request
+    // get data
+    // insert into main -> div.picture
+    // pages -> on scroll ?
+    // masonry
+    // input fields for custom params
+  });
+})();
