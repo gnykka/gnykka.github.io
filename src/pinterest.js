765539841b063ab494e0366d34de29c9644d1f04
Nataliya Stepanova <gnykka@gmail.com>
2019-04-01 15:43:19 +0300

    Get pinterest photos for user

--- src/pinterest.js
+++ src/pinterest.js
@@ -1,12 +1,43 @@
 (() => {
   window.addEventListener('load', () => {
-    const main = document.getElementsByTagName('main')[0];
+    const pictures = document.querySelector('.pictures');
+    const baseUrl = 'https://api.pinterest.com/v3/pidgets/users/';
+    const defaultUser = 'townofdragons';
 
-    // make request
-    // get data
-    // insert into main -> div.picture
-    // pages -> on scroll ?
-    // masonry
-    // input fields for custom params
+    function getNewPins(user) {
+      fetch(`${baseUrl}${user}/pins/`)
+        .then(data => data.json())
+        .then(({ data }) => {
+          pictures.innerHTML = '';
+          data.pins.forEach((pin) => {
+            const picture = document.createElement('div');
+            picture.classList.add('picture');
+
+            const img = document.createElement('img');
+            img.src = pin.images['564x'].url;
+
+            picture.appendChild(img);
+            pictures.appendChild(picture);
+          });
+        });
+    }
+
+    getNewPins(defaultUser);
+
+    const form = document.querySelector('.form');
+    const input = form.elements['username'];
+    const button = form.elements['submit'];
+
+    input.value = defaultUser;
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      getNewPins(input.value);
+    });
+    button.addEventListener('click', (event) => {
+      getNewPins(input.value);
+    });
+
+    // add cover while loading
+    // masonry layout ?
   });
 })();
